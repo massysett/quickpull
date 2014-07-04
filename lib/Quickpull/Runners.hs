f585398bc4f15cb15b0627afeb6036cea3ccc791
Omari Norman <omari@smileystation.com>
2014-07-03 21:51:52 -0400

    WIP

---
+++ lib/Quickpull/Runners.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE RankNTypes #-}
+module Quickpull.Runners where
+
+import Test.QuickCheck
+  ( Testable, Result(..), quickCheckResult, quickCheckWithResult,
+    Args )
+import Quickpull.Types
+import Quickpull.Formatting
+import Quickpull.Render
+import Data.List (foldl')
+import System.Exit
+
+quickcheckTree
+  :: (forall a. Testable a => a -> IO Result)
+  -> TestTree
+  -> IO [Result]
+quickcheckTree run = go []
+  where
+    go soFar (TestTree l n) = do
+      putStr . titles $ l : soFar
+      case n of
+        Group tt -> fmap concat . mapM (go (l : soFar)) $ tt
+        Test t -> fmap (:[]) $ run t
+
+quickcheckDecree
+  :: (forall a. Testable a => a -> IO Result)
+  -> Decree
+  -> IO [Result]
+quickcheckDecree run (Decree m i) = do
+  putStr . metaLine $ m
+  case i of
+    Single a -> fmap (:[]) . run $ a
+    Multi t -> quickcheckTree run t
+
+
+summarize :: [Result] -> Summary
+summarize = foldl' f (Summary 0 0 0 0)
+  where
+    f s r = case r of
+      Success {} -> s { success = succ (success s) }
+      GaveUp {} -> s { gaveUp = succ (gaveUp s) }
+      Failure {} -> s { failure = succ (failure s) }
+      NoExpectedFailure {} -> s
+        { noExpectedFailure = succ (noExpectedFailure s) }
+
+exitCode :: Summary -> ExitCode
+exitCode s
+  | gaveUp s == 0 && failure s == 0 &&
+    noExpectedFailure s == 0 = ExitSuccess
+  | otherwise = ExitFailure 1
+
+withDecree
+  :: (Decree -> forall a. Testable a => a -> IO Result)
+  -> [Decree]
+  -> IO [[Result]]
+withDecree f ds = mapM g ds
+  where
+    g d = quickcheckDecree (f d) d
+
+runTests
+  :: (Decree -> forall a. Testable a => a -> IO Result)
+  -> [Decree]
+  -> IO ()
+runTests f ds = do
+  rs <- fmap concat $ withDecree f ds
+  let s = summarize rs
+      c = exitCode s
+  putStr $ summary s
+  exitWith c
+
+defaultMain :: [Decree] -> IO ()
+defaultMain = runTests (const quickCheckResult)
+
+defaultMainWith :: Args -> [Decree] -> IO ()
+defaultMainWith a = runTests (const (quickCheckWithResult a))
