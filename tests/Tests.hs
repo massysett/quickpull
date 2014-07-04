63e4c3340c5615a2b4f7637a6316f9f000c655a2
Omari Norman <omari@smileystation.com>
2014-07-04 11:33:44 -0400

    WIP

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -5,4 +5,7 @@ import Quickpull.Laws
 import Test.QuickCheck
 
 proptree_maybeMonadLaws :: TestTree
-proptree_maybeMonadLaws = monad arbitrary arbitrary id
+proptree_maybeMonadLaws = monad arbitrary arbitrary (return same)
+  where
+    same :: Maybe Int -> Maybe Int
+    same = id
